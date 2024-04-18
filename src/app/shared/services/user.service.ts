ef5ac7d5359cb3787b2e92e854d0d1d166546c7f
debarshi-git <165477234+debarshi-git@users.noreply.github.com>
2024-04-18 23:24:47 +0530

    create user profile...

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  public user_url = "http://localhost:3000/user/";
+  constructor(private http:HttpClient, private apiService:ApiService) { }
+
+  //get individual data
+  getUserData(user_id : any){
+    return this.apiService.get(this.user_url+user_id);
+  }
+
+  //update data by user id,
+  // to get data observable is not needed, but when we put it.. it is needed..
+
+  updateUserData(user_id:any, user_dto:any):Observable<any>{
+    return this.apiService.put(this.user_url+user_id , user_dto);
+  }
 }
