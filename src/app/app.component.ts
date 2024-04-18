48a2ee763905972c5c5c51ade912df4e97015ca8
debarshi-git <165477234+debarshi-git@users.noreply.github.com>
2024-04-19 00:12:26 +0530

    using host listener fixed footer...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layouts/header/header.component';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
@@ -12,5 +12,21 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  screenHeight:any;
+  screenWidth:any;
+  footerMaxHeight!:number;
   title = 'angularEcom';
+
+  constructor(){
+    this.getScreenSize(event);
+  }
+
+  //decoretor..
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event:any){
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    //console.log(this.screenHeight, this.screenWidth)
+    this.footerMaxHeight = this.screenHeight -160;
+  }
 }
