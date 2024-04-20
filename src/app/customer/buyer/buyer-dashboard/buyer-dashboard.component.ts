d6b000bc7e5fe2debff18a291f3127bc81840a96
debarshi-git <165477234+debarshi-git@users.noreply.github.com>
2024-04-20 12:30:33 +0530

    update buyer dashboard, and customer service..

--- src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
+++ src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-buyer-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './buyer-dashboard.component.html',
   styleUrl: './buyer-dashboard.component.css'
 })
-export class BuyerDashboardComponent {
+export class BuyerDashboardComponent implements OnInit{
+  all_products:any;
+  show_checkout:boolean=false;
+
+  constructor(private router:Router, private customerService:CustomerService){}
+
+  ngOnInit(): void {
+    this.getAllProducts()
+    
+  }
+  getAllProducts(){
+    this.customerService.allProduct().subscribe(data =>{
+      this.all_products=data;
+      console.log(data);
+    },error=>{
+      console.log("getAllProducts error", error);
+    })
+  }
+
+  buyProduct(id:number){
+    this.show_checkout=true;
+    this.customerService.quickBuyProduct(id);
+    this.router.navigateByUrl('/checkout');
+  }
+
+  addToCart(){
+    alert("this is show case");
+  }
 
 }
--- src/app/customer/services/customer.service.ts
+++ src/app/customer/services/customer.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  //an observable sequence that ides the source sequence...
+private Single_product_id = new BehaviorSubject(null);
+current_product= this.Single_product_id.asObservable();
+  public user_url = 'http://localhost:3000/user/';
+  public product_url = 'http://localhost:3000/products/';
+  public order_url = 'http://localhost:3000/orders/';
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
+  //here we are creating metoods for all purposes... and observe the data...
+
+  allProduct():Observable<any>{
+    return this.apiService.get(this.product_url);
+  }
+  quickBuyProduct(product_id:any){
+    this.Single_product_id.next(product_id);
+  }
+  individualProduct(id:any){
+    return this.apiService.get(this.product_url+id);
+  }
+  userDetail(id:any){
+    return this.apiService.get(this.user_url+id);
+  }
+  insertNewOrder(order_dto:any):Observable<any>{
+    return this.apiService.post(this.order_url,order_dto);
+  }
+  orderDashBoardData():Observable<any>{
+    return this.apiService.get(this.order_url);
+  }
+  productDashBoardData():Observable<any>{
+    return this.apiService.get(this.product_url);
+  }
 }
