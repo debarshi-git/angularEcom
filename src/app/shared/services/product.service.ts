ae781e389e1250ba6f43406a2b453b4ba5920313
debarshi-git <165477234+debarshi-git@users.noreply.github.com>
2024-04-23 11:44:09 +0530

    product component and service..

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+public Product_Url = "http://localhost:3000/products/"
+  constructor(private httpClient:HttpClient, private apiService:ApiService) { }
 
-  constructor() { }
+  allProduct():Observable<any>{
+    return this.apiService.get(this.Product_Url);
+  }
+
+  addNewProduct(Product_dto:any):Observable<any>{
+    return this.apiService.post(this.Product_Url,Product_dto);
+  }
+
+  //as it is a single product.. so no need to observe..
+  singleProduct(id:any){
+    return this.apiService.get(this.Product_Url+id);
+  }
+
+  updateProduct(id:any,Product_dto:any):Observable<any>{
+    return this.apiService.put(this.Product_Url+id,Product_dto);
+  }
+
+  deleteProduct(id:any):Observable<any>{
+    return this.apiService.delete(this.Product_Url+id);
+  }
 }
